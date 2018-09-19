d0d67464b4721b527756dae6a6f27efc045ff981
Sergii Yavorskiy <oleg@gr-development.com>
2018-09-19 19:59:16 +0300

    first init commit

---
+++ templates/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en"  >
+
+<head>
+	<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="noindex, nofollow" />
+
+    <!-- <link href="app/dist/img/favicon.png" rel="shortcut icon"  > -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./app/dist/css/vendor.css">
+    <link rel="stylesheet" type="text/css" href="./app/dist/css/style.css">
+    <!--<script type="text/javascript" src="./app/dist/js/vendor.js"></script>-->
+    <script type="text/javascript" src="./app/dist/js/dist.js"></script>
+
+	<title><?php echo ' My Title ';?></title>
+
+
+</head>
+
+<body id="body" class="wrapper">
+    <header class="header">
+        <div class="container-fluid header-top">
+            <div class="row">
+                <div class="col-1 ">
+                    <div class="menuButton">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                </div>
+
+                <div class="col-2 ">
+                    <div class="logo"></div>
+                </div>
+                <div class="col-6">
+                    <ul class="menuTop">
+                        <li class="menu-item"><a href="">Услуги</a></li>
+                        <li class="menu-item"><a href=""><a href="">о компании</a></li>
+                        <li class="menu-item"><a href=""><a href="">команда</a></li>
+                        <li class="menu-item"><a href=""><a href="">Медиа-центр</a></li>
+                        <li class="menu-item"><a href=""><a href="">проекты</a></li>
+                        <li class="menu-item"><a href=""><a href="">Контакты</a></li>
+                    </ul>
+                </div>
+                <div class="col-1 language">
+                    <label for="select"></label>
+                    <select>
+
+                        <option selected>
+                            RU
+                        </option>
+                        <option>
+                            EN
+                        </option>
+                    </select>
+                </div>
+                <div class="col-1 phones">
+                    <div class="row">
+                        <div class="col">
+                            <a href="+84955140594">8 495 514-05-94</a>
+                        </div>
+                        <div class="col">
+                            <a href="+84955140594">8 800 100-82-47</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
